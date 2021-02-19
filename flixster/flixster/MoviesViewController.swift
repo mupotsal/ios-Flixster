7450aacb70a2085b88009a0caca41d8683a982b5
liberty Mupotsa <mupotsal@berea.edu>
2021-02-19 10:35:48 -0500

    done with required stuff

--- flixster/flixster/MoviesViewController.swift
+++ flixster/flixster/MoviesViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AlamofireImage
+
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -50,10 +52,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
-        cell.textLabel!.text = title
+        let synopsis = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)!
+        cell.posterView.af_setImage(withURL: posterURL)
         return cell
     }
     

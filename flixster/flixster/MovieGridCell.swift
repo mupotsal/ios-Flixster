d65d43c0717fa55ab6cb52dbbb19ad2ae7dde744
liberty Mupotsa <mupotsal@berea.edu>
2021-02-26 03:09:25 -0500

    Done with Collection View SetUp

---
+++ flixster/flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Liberty Mupotsa on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}

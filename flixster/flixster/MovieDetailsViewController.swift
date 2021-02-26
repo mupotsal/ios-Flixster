e711df3b72eaf0bcb3111a712fda9bead84f0d9f
liberty Mupotsa <mupotsal@berea.edu>
2021-02-25 23:58:00 -0500

    added the screen

--- flixster/flixster/MovieDetailsViewController.swift
+++ flixster/flixster/MovieDetailsViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
     
     var movie: [String:Any]!
     override func viewDidLoad() {
@@ -15,6 +20,16 @@ class MovieDetailsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         print(movie["title"])
+        titleLabel.text = movie["title"] as! String
+        synopsisLabel.text = movie["overview"] as! String
+        
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)!
+        posterView.af_setImage(withURL: posterURL)
+        
+    
     }
     
 
